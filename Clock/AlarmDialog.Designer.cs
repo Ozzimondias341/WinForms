a6a20d596c2d049c498b8e88588cc7c8f64f46d2
Максим <@DESKTOP-VML76U3>
2026-01-12 20:21:25 +0300

    TO DO:  CLB

--- Clock/AlarmDialog.Designer.cs
+++ Clock/AlarmDialog.Designer.cs
@@ -31,6 +31,8 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.dtpTime = new System.Windows.Forms.DateTimePicker();
             this.checkBoxUseDate = new System.Windows.Forms.CheckBox();
+            this.labelWeekdays = new System.Windows.Forms.Label();
+            this.clbWeekdays = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // dateTimePicker1
@@ -64,11 +66,40 @@
             this.checkBoxUseDate.Text = "На определённую дату";
             this.checkBoxUseDate.UseVisualStyleBackColor = true;
             // 
+            // labelWeekdays
+            // 
+            this.labelWeekdays.AutoSize = true;
+            this.labelWeekdays.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWeekdays.Location = new System.Drawing.Point(13, 86);
+            this.labelWeekdays.Name = "labelWeekdays";
+            this.labelWeekdays.Size = new System.Drawing.Size(130, 25);
+            this.labelWeekdays.TabIndex = 3;
+            this.labelWeekdays.Text = "Дни недели";
+            // 
+            // clbWeekdays
+            // 
+            this.clbWeekdays.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clbWeekdays.FormattingEnabled = true;
+            this.clbWeekdays.Items.AddRange(new object[] {
+            "Пн",
+            "Вт",
+            "Ср",
+            "Чт",
+            "Пт",
+            "Сб",
+            "Вс"});
+            this.clbWeekdays.Location = new System.Drawing.Point(13, 115);
+            this.clbWeekdays.Name = "clbWeekdays";
+            this.clbWeekdays.Size = new System.Drawing.Size(120, 123);
+            this.clbWeekdays.TabIndex = 4;
+            // 
             // AlarmDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(503, 360);
+            this.Controls.Add(this.clbWeekdays);
+            this.Controls.Add(this.labelWeekdays);
             this.Controls.Add(this.checkBoxUseDate);
             this.Controls.Add(this.dtpTime);
             this.Controls.Add(this.dateTimePicker1);
@@ -84,5 +115,7 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.DateTimePicker dtpTime;
         private System.Windows.Forms.CheckBox checkBoxUseDate;
+        private System.Windows.Forms.Label labelWeekdays;
+        private System.Windows.Forms.CheckedListBox clbWeekdays;
     }
 }
